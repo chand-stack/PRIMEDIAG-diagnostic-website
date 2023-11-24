09320dd5d29e3c57c41f0e397946f7d583ec166e
chand-stack <chand.stack@gmail.com>
2023-11-24 20:47:15 +0600

    Chore: responsive footer section added

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../../Shared/Banner/Banner";
+import Footer from "../../Shared/Footer/Footer";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
         <title>PrimeDiag</title>
       </Helmet>
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
