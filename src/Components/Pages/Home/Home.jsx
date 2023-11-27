b4452b1bb4a13872dbf6e35ea359db2ff2b4e3f5
chand-stack <chand.stack@gmail.com>
2023-11-27 16:16:47 +0600

    Chore: newsletter section added

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import Banner from "../../Shared/Banner/Banner";
 import About from "../../Shared/About/About";
 import Recommendation from "../../Shared/Recommend/Recommendation";
+import NewsLetter from "../../Shared/NewsLetter/NewsLetter";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Banner></Banner>
       <About></About>
       <Recommendation></Recommendation>
+      <NewsLetter></NewsLetter>
     </div>
   );
 };
---
+++ src/Components/Shared/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,35 @@
+import bg from "../../../assets/breadcrumb-bg.jpg";
+import { FaBell } from "react-icons/fa";
+const NewsLetter = () => {
+  return (
+    <div className="container mx-auto md:px-10 ">
+      <div
+        className="h-52 md:h-64 "
+        style={{
+          backgroundImage: `url(${bg})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="h-full w-full bg-[#34cceb] bg-opacity-80 lg:flex text-center justify-evenly items-center">
+          <h1 className="text-3xl md:text-5xl font-bold text-white">
+            Subscribe to Our <br /> News Letter
+          </h1>
+          <div className="join w-[400px]">
+            <input
+              className="input w-full h-14 input-bordered join-item"
+              placeholder="Enter Email Address"
+            />
+            <button className="btn join-item h-14 rounded-r-full bg-orange-400 text-white">
+              Subscribe
+              <FaBell className="text-2xl" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsLetter;
